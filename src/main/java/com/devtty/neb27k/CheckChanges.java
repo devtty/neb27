b45650f3cea331514fff609a770e04bc830242c4
Denis Renning <denis@devtty.de>
2016-01-07 12:25:59 +0100

    check main page closes #1

--- src/main/java/com/devtty/neb27k/CheckChanges.java
+++ src/main/java/com/devtty/neb27k/CheckChanges.java
@@ -1,6 +1,9 @@
 package com.devtty.neb27k;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,6 +15,12 @@ import org.quartz.JobExecutionException;
 import org.quartz.SchedulerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import twitter4j.Paging;
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 
 /**
  *
@@ -29,44 +38,89 @@ public class CheckChanges implements Job{
     public void execute(JobExecutionContext jec) throws JobExecutionException {
         
         try {
-            Document doc = Jsoup.connect("http://www.neb.de/service/baustellenfahrplanaenderungen/").get();
-            Elements line = doc.select(".news");
-
             List<String> changes = (List<String>) jec.getScheduler().getContext().get(Constants.CHANGES);
-
-            for(Element e : line){
-                if(e.text().equals(Constants.NO_CHANGES)){
-                    logger.debug("No traffic changes");
-                    return;
-                }else{                    
-                    logger.debug("handle change--- IMPLEMENT!");
-                    //tweet if not already sent in the last 24h
-                }
+            logger.debug("Changes Amount: {}", changes.size());
+            if(changes.size()>0){
+                notifyClients(jec);
             }
-            
-            
-            /*
-            try {
-            Twitter twitter = TwitterFactory.getSingleton();
-            
-            Query query = new Query("#neb27 OR #Regionalbahndamenklub OR #heidekrautbahn since:2016-01-03");
-            QueryResult result = twitter.search(query);
-            
-            for(Status status : result.getTweets()){
-            logger.info("test" + status.getUser().getScreenName() + ": " + status.getText());
-            if(!status.isRetweetedByMe()){
-            logger.debug("Retweet status " + status.getId());
-            //TODO productive !!!! twitter.retweetStatus(status.getId());
-            }
-            }
-            
-            
-            } catch (TwitterException ex) {
-            logger.error(ex.getMessage());
-            }*/
-        } catch (IOException | SchedulerException ex) {
+        } catch (SchedulerException | TwitterException ex) {
             logger.error(ex.getMessage());
         }
+    }
+
+    private boolean checkMainPage(JobExecutionContext jec) throws SchedulerException, IOException {
+        boolean change = false;
+        
+        Document doc = Jsoup.connect(Constants.URL_MAIN).get();
+        Elements line = doc.select(Constants.CSS_SEARCH_WARNING);
+        List<String> changes = (List<String>) jec.getScheduler().getContext().get(Constants.CHANGES);
+        for(Element e : line) {
+            logger.debug("MainPageLine: {}", e.text());
+            if(e.text().contains(Constants.RB27)){
+                changes.add(e.text());
+                change = true;
+            }
+        }
+        return change;
+    }
+    
+    private boolean checkChangesPage(JobExecutionContext jec) throws SchedulerException, IOException {
+        boolean change = false;
+        Document doc = Jsoup.connect(Constants.URL_CHANGES).get();
+        Elements line = doc.select(Constants.CSS_SEARCH_NEWS);
+        List<String> changes = (List<String>) jec.getScheduler().getContext().get(Constants.CHANGES);
+        for (Element e : line) {
+            if (e.text().equals(Constants.NO_CHANGES)) {
+                logger.debug("No traffic changes");
+                change =  true;
+            } else {
+                logger.debug("handle change--- IMPLEMENT!");
+                //tweet if not already sent in the last 24h
+            }
+        }
+        return change;
+    }
+
+    private void notifyClients(JobExecutionContext jec) throws SchedulerException, TwitterException {
+        
+        List tweetsToday = getTweetsToday();
+        
+        List<String> changes = (List<String>) jec.getScheduler().getContext().get(Constants.CHANGES);
+        
+        for(String c : changes){
+            if(c.length()>130){
+                c = c.substring(6, 145);
+            }
+            
+            if(!tweetsToday.contains(c)){
+                Twitter twitter = TwitterFactory.getSingleton();
+                
+                Status update = twitter.updateStatus(c);
+                jec.getScheduler().getContext().put(Constants.LAST_TWEET, update.getId());
+                jec.getScheduler().getContext().put(Constants.CHANGES, new ArrayList<>());
+                
+                logger.debug("Tweet: {}", c);
+            }   
+        }
+    }
+
+    private List<String> getTweetsToday() throws TwitterException {
+        List<String> tweetsToday = new ArrayList<>();
+
+        Twitter twitter = TwitterFactory.getSingleton();
+
+        Calendar midnight = new GregorianCalendar();
+        midnight.set(Calendar.HOUR_OF_DAY, 0);
+        midnight.set(Calendar.MINUTE, 0);
+        midnight.set(Calendar.SECOND, 0);
+        midnight.set(Calendar.MILLISECOND, 0);
+
+        ResponseList<Status> lastTweets = twitter.getHomeTimeline(new Paging(20));
+        lastTweets.stream().filter((s) -> (!s.isRetweet() && s.getCreatedAt().after(midnight.getTime()))).forEach((s) -> {
+            tweetsToday.add(s.getText());
+        });
+
+        return tweetsToday;
     }
     
 }
--- src/main/java/com/devtty/neb27k/Constants.java
+++ src/main/java/com/devtty/neb27k/Constants.java
@@ -6,8 +6,14 @@ package com.devtty.neb27k;
  */
 public class Constants {
 
+    public final static String RB27 = "RB27";
     public final static String CHANGES    = "changes";
     public final static String LAST_TWEET = "lasttweet";
     public final static String NO_CHANGES = "Keine Nachrichten verfügbar.";
     
+    public final static String CSS_SEARCH_NEWS = ".news";
+    public final static String CSS_SEARCH_WARNING = ".achtung";
+    
+    public final static String URL_MAIN = "http://www.neb.de/";
+    public final static String URL_CHANGES = "http://www.neb.de/service/baustellenfahrplanaenderungen/";
 }
