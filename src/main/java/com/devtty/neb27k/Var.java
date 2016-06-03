cb0f3ceb1fb993205260a2a8cac15e437f78b270
Denis Renning <denis@devtty.de>
2016-06-03 09:18:13 +0200

    workaround (can't ssh to openshift currently)

--- src/main/java/com/devtty/neb27k/Var.java
+++ src/main/java/com/devtty/neb27k/Var.java
@@ -1,15 +1,17 @@
 package com.devtty.neb27k;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import javax.annotation.PostConstruct;
-import javax.ejb.Startup;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.servlet.ServletContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import twitter4j.Paging;
-import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -27,6 +29,9 @@ public class Var {
     private long lastTweet = 684360872010059776L;
     private List<String> changes;
     private String tweetas;
+    private String health;
+    
+    @Inject ServletContext servletContext;
     
     public long getLastTweet() {
         return lastTweet;
@@ -63,6 +68,39 @@ public class Var {
             lastTweet = twitter.getHomeTimeline(new Paging(1)).get(0).getId();
         } catch (TwitterException ex) {
             logger.error(ex.getErrorMessage());
+        } catch (IllegalStateException isx){
+            health = health();
         }
     }
+    
+    private String health(){
+        String s = "";
+        
+        String ht = System.getenv("hafasAccessToken");
+        String lt = System.getenv("lametricAccessToken");
+        
+        if(ht!=null && !ht.isEmpty()){
+            s = s + "hafas OK " + ht.substring(0,3);
+        }
+        
+        if(lt!=null && !lt.isEmpty()){
+            s = s + " lametric OK " + lt.substring(0,3);
+        }
+            
+        if((new File(servletContext.getRealPath("/WEB-INF/classes/twitter4j.properties"))).exists()){
+            s = s + " twitter Properties OK";
+        }
+        
+        return s;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+    
+    
 }
